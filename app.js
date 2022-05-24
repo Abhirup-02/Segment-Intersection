7f995aa0cf65c5a08207a0ca23b8b0615c6d86cb
Abhirup-02 <92736753+Abhirup-02@users.noreply.github.com>
2022-05-25 02:20:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ document.onmousemove=(event)=>{
     mouse.y=event.y
 }
 document.ontouchmove=(event)=>{
-    Touch.x=event.x
-    Touch.y=event.y
+    Touch.clientX=event.x
+    Touch.clientY=event.y
 }
 animate()
 
@@ -28,10 +28,10 @@ function animate(){
     A.y=mouse.y-Math.sin(angle)*radius
     B.x=mouse.x-Math.cos(angle)*radius
     B.y=mouse.y+Math.sin(angle)*radius
-    A.x=Touch.x+Math.cos(angle)*radius
-    A.y=Touch.y-Math.sin(angle)*radius
-    B.x=Touch.x-Math.cos(angle)*radius
-    B.y=Touch.y+Math.sin(angle)*radius
+    A.x=Touch.clientX+Math.cos(angle)*radius
+    A.y=Touch.clientY-Math.sin(angle)*radius
+    B.x=Touch.clientX-Math.cos(angle)*radius
+    B.y=Touch.clientY+Math.sin(angle)*radius
     angle+=0.02
 
 
